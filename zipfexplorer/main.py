aeeaef0b61ddbf9d4cb79f02999641ec279cdb75
stcoats <stevencoats@gmail.com>
2019-08-09 09:38:42 +0000

    revert

--- zipfexplorer/main.py
+++ zipfexplorer/main.py
@@ -216,7 +216,7 @@ def update(selected=None):
     data = get_data(t1, t2)
     source.data = source.from_df(data[['word','rank_x','rank_y','freq_x','freq_y','sum_x','sum_y','rel_x','rel_y','rel_diff','LL','pval']])
     selection = np.array(load_ticker(t1)[~load_ticker(t1)["word"].isin(ss[0:int(stopwords_1.value)])]["freq"].astype(float))
-    left_lin.title.text = '%s, Gini coef. = %s' % (t1, round(gini(selection),3)) + ', TTR = %s' round(len(selection)/sum(selection),3)
+    left_lin.title.text = '%s, Gini coef. = %s' % (t1, round(gini(selection),3)) + ', TTR = %s' % round(len(selection)/sum(selection),3)
     
     #left_lin.title.text = '%s, TTR = %s' % (t1, round(len(load_ticker(t1)[~load_ticker(t1)["word"].isin(ss[0:int(stopwords_1.value)])]["freq"].astype(float))/load_ticker(t1)[~load_ticker(t1)["word"].isin(ss[0:int(stopwords_1.value)])]["freq"].astype(float)).sum(),3) +\
     #', Gini = %s' % round(gini(load_ticker(t1)[~load_ticker(t1)["word"].isin(ss[0:int(stopwords_1.value)])]["freq"].astype(float)),3) +\
