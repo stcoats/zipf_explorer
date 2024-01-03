0b248a45d25cc474981b1a297c1490b86f00da57
stcoats <32770789+stcoats@users.noreply.github.com>
2024-01-03 13:24:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def webserver():
     app_directory = os.path.abspath("zipfexplorer")
 
     # Use bokeh serve to serve the app
-    main(["bokeh", "serve", "zipfexplorer", "--port", "0", "--allow-websocket-origin", "zipfexplorer-zipf7.rahtiapp.fi:8080", "--address", "0.0.0.0", "--use-xheaders"])
+    main(["bokeh", "serve", "zipfexplorer", "--port", "0", "--allow-websocket-origin", "zipfexplorer-zipf9.rahtiapp.fi:8080", "--address", "0.0.0.0", "--use-xheaders"])
 
 if __name__ == '__main__':
     webserver()
