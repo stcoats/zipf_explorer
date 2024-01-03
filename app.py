50df9275a1f65835505781889b2b47f37e0de8a0
stcoats <32770789+stcoats@users.noreply.github.com>
2024-01-03 08:46:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from bokeh.command.bootstrap import main
 
 main(["bokeh", "serve", "--show", "./zipfexplorer/main.py"])
+
+import os
+from bokeh.command.bootstrap import main
+
+def webserver():
+    # Get the path to the directory containing your Bokeh application
+    # Not mandatory, but clearer
+    app_directory = os.path.abspath("zipfexplorer")
+
+    # Use bokeh serve to serve the app
+    main(["bokeh", "serve", app_directory, "--port", "8080", "--allow-websocket-origin", "zipf5-zipf5.rahtiapp.fi", "--address", "0.0.0.0", "--use-xheaders"])
+
+if __name__ == '__main__':
+    webserver()
